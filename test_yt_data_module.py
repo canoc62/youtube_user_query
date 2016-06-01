274fb41ac842127f444675f87b666381855c5d26
Christopher Cano <canoc4262@gmail.com>
2016-06-01 13:43:23 -0700

    Turn yt_data to module.

---
+++ test_yt_data_module.py
@@ -0,0 +1,9 @@
+import yt_data as youtube_user_query
+
+youtube = youtube_user_query.build_youtube_service(
+	'AIzaSyAM5ytZfxRt9lAJBZkCDG8aFATE9030mAg'
+)
+
+youtube_username = "RealRobReport"
+youtube_user_query.get_channel_info(youtube, youtube_username)
+
--- yt_data.py
+++ yt_data.py
@@ -1,25 +1,30 @@
 from apiclient.discovery import build
 import json
 
-# developerKey - Google Developer API Key
-youtube_service = build('youtube', 'v3', developerKey="")
+# devKey - Google Developer API Key
 
-user_list = {
-    "users": [
+def build_youtube_service(devKey):
+    api = 'youtube' 
+    api_version = 'v3'
+    youtube_service = build(api, api_version, developerKey=devKey)
+    return youtube_service
 
-    ]
-}
+def get_channel_info(youtube_service, youtube_username): 
 
+    #user_list = {
+    #   "users": [
 
-def get_channel_info(youtube_service, username): 
+    #    ]
+    #}
+
     results = youtube_service.channels().list(
         part="snippet, statistics",
-        forUsername=username
+        forUsername=youtube_username
     ).execute()
 
     user_object = {
         "meta": {
-            "username": username
+            "username": youtube_username
         },
         "channels": [
 
@@ -39,20 +44,25 @@ def get_channel_info(youtube_service, username):
     
         user_object["channels"].append(channel)
 
-    user_list["users"].append(
-        user_object
-    )
+    #user_list["users"].append(
+    #    user_object
+    #)
 
-    youtube_file = 'youtube_%s.json' %(username)
+    youtube_file = 'youtube_%s.json' %(youtube_username)
     f = open(youtube_file, 'w')
-    f.write(json.dumps(user_list, indent=4, sort_keys=False))
+    f.write(json.dumps(user_object, indent=4, sort_keys=False))
     f.close()
          
 
 # Test
+'''
+youtube = build_youtube_service(
+    'AIzaSyAM5ytZfxRt9lAJBZkCDG8aFATE9030mAg'
+)
 
 username = "RealRobReport"
-get_channel_info(youtube_service, username)
+get_channel_info(youtube, username)
+'''
 
 
 
